e68a70b57ba737fca36986731d0b5ea57afe8358
victoria-parker <victoriaparkerb@gmail.com>
2023-12-16 19:01:56 +0100

    todolist and items models added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ToDoList(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    todoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE, related_name="todolist",null=True)
+    text = models.CharField(max_length = 300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
